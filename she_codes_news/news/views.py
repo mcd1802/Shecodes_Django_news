c63e958f915418f35e9b41bfecb3a76b47132f12
mcd1802 <monica.dewi@bhp.com>
2021-08-16 23:21:03 +1000

    Django She Codes News-1
    Django She Codes News-1

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -1,6 +1,7 @@
 from django.views import generic
+from django.urls import reverse_lazy
 from .models import NewsStory
-
+from .forms import StoryForm
 
 class IndexView(generic.ListView):
     template_name = 'news/index.html'
@@ -14,3 +15,14 @@ class IndexView(generic.ListView):
         context['latest_stories'] = NewsStory.objects.all()[:4]
         context['all_stories'] = NewsStory.objects.all()
         return context
+
+class StoryView(generic.DetailView):
+    model = NewsStory
+    template_name = 'news/story.html'
+    context_object_name = 'story'
+
+class AddStoryView(generic.CreateView):
+    form_class = StoryForm
+    context_object_name = 'storyForm'
+    template_name = 'news/createStory.html'
+    success_url = reverse_lazy('news:index')
